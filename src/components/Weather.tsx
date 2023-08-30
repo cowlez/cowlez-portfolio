bfb42f91606b0ea76162c83e8e2dd408d7096ca9
Reizy <malfunzionatoo@proton.me>
2023-08-30 03:28:51 +0200

    .

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Icon } from "react-icons/fa";
+import { Icon } from "@fortawesome/react-fontawesome";
 
 const Weather = () => {
   const [weather, setWeather] = useState({
